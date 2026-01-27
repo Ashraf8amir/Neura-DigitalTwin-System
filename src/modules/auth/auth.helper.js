cf36aa908af2c454e7e27165da67b3ca35030d89
Ashraf Samir <achrafsamir808@gmail.com>
2026-01-28 01:59:17 +0200

    Edit arctic

--- src/modules/auth/auth.helper.js
+++ src/modules/auth/auth.helper.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
-const { generateState, generateCodeVerifier } = require('arctic');
+const { generateState, generateCodeVerifier } = await require('arctic');
 const AppError = require('../../core/appError.js');
 const httpStatus = require('../../core/httpStatus.js');
 const config = require('../../config/config');
