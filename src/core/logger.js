44948f9136627d4423da050aa6672af8d9239fd4
Ashraf Samir <achrafsamir808@gmail.com>
2026-01-28 02:34:02 +0200

    Refactor logger configuration

--- src/core/logger.js
+++ src/core/logger.js
@@ -2,7 +2,6 @@ const winston = require('winston');
 const path = require('path');
 
 const logger = winston.createLogger({
-  
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.errors({ stack: true }),
@@ -11,26 +10,28 @@ const logger = winston.createLogger({
   ),
   level: 'info',
   transports: [
-    new winston.transports.File({
-      filename: path.join(__dirname, '../../logs/error.log'),
-      level: 'error',
-      maxsize: 5242880, // 5MB
-      maxFiles: 5
-    }),
-    new winston.transports.File({
-      filename: path.join(__dirname, '../../logs/combined.log'),
-      maxsize: 5242880,
-      maxFiles: 5
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
     })
   ]
 });
 
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
-    )
+  
+  logger.add(new winston.transports.File({
+    filename: path.join(__dirname, '../../logs/error.log'),
+    level: 'error',
+    maxsize: 5242880,
+    maxFiles: 5
+  }));
+
+  logger.add(new winston.transports.File({
+    filename: path.join(__dirname, '../../logs/combined.log'),
+    maxsize: 5242880,
+    maxFiles: 5
   }));
 }
 
